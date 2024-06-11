bd4e8fccf58bfdc862a6e7eb3c14834a033d0d49
evellynbarbosa <160596454+evellynbarbosa@users.noreply.github.com>
2024-06-11 11:45:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById('todo-form').addEventListener ("submit", function(e) { 
+    e.preventDefault();
+    const input=document.
+    getElementById("todo-input" );
+ addtodo ( input.value);
+ input.value= " ";
+});
+function addTodo(tarefa){
+    const list = document.
+    getElementById("todo-list");
+    const listItem = document.
+    createElement("li");
+    listItem.textContent = tarefa;
+    list.appendChild(listItem);
+}
